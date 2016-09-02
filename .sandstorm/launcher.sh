027a3deda697ee197139e9f5da70dbf07ab4e426
Abhinand <abhinandh@gmail.com>
2016-09-02 00:37:52 -0700

    Getting started with packaging for sandstorm.

---
+++ .sandstorm/launcher.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Create a bunch of folders under the clean /var that php and nginx expect to exist
+mkdir -p /var/lib/nginx
+mkdir -p /var/lib/php5/sessions
+mkdir -p /var/log
+mkdir -p /var/log/nginx
+# Create a data directory for miniflux
+mkdir -p /var/miniflux/data/
+# Wipe /var/run, since pidfiles and socket files from previous launches should go away
+# TODO someday: I'd prefer a tmpfs for these.
+rm -rf /var/run
+mkdir -p /var/run
+
+# Spawn php
+/usr/sbin/php5-fpm --nodaemonize --fpm-config /etc/php5/fpm/php-fpm.conf &
+# Wait until php has bound its sockets, indicating readiness
+while [ ! -e /var/run/php5-fpm.sock ] ; do
+    echo "waiting for php5-fpm to be available at /var/run/php5-fpm.sock"
+    sleep .2
+done
+
+# Start nginx.
+/usr/sbin/nginx -c /opt/app/.sandstorm/service-config/nginx.conf -g "daemon off;"
